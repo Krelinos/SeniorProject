d269208587bf46ad4949f3e489819f723d70b89d
Krelinos <30482246+Krelinos@users.noreply.github.com>
2023-04-14 18:28:42 -0500

    Raul's progress, built with Ant instead of Maven

---
+++ src/hellofx/classes/seniorproject/HelloApplication.java
@@ -0,0 +1,119 @@
+package seniorproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.IOException;
+
+import static seniorproject.HelloController.*;
+
+public class HelloApplication extends Application {
+    static double[] xchords = new double[10000];
+    static double[] ychords = new double[10000];
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 950, 750);
+        stage.setTitle("Wave Form Reader");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+
+    public static void printNodeList(NodeList nodeList) {
+
+
+
+        /////////////LOOP THROUGH XML FILE AND CREATE AN ARRAY WITH ALL X CHORDS AND ANOTHER ARRAY WITH ALL Y CHORDS
+
+        for (int count = 0; count < nodeList.getLength(); count++) {
+            Node elemNode = nodeList.item(count);
+            if (elemNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                {
+                    NamedNodeMap nodeMap = elemNode.getAttributes();
+
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Node node = nodeMap.item(i);
+                        //if the node name is x or y
+
+                        /////////////////////////////////////////CHECK FOR TAG "X"
+                        if (node.getNodeName() == "x") {
+
+                            //System.out.println("attr name : " + node.getNodeName());
+                            // System.out.println("attr value x  : " + node.getNodeValue());
+                            //PUT THE VALUE INTO THE ARRAY as a double from string
+                            xchords[b] = Double.parseDouble(node.getNodeValue());
+                            b++;
+
+                        }
+                        /////////////////////////////////////////CHECK FOR TAG Y
+                        if (node.getNodeName() == "y") {
+
+                            //System.out.println("attr name : " + node.getNodeName());
+                            //System.out.println("attr value y  : " + node.getNodeValue());
+                            //PUT THE VALUE INTO THE ARRAY as a double from string
+                            ychords[n] = Double.parseDouble(node.getNodeValue());
+                            n++;
+                        }
+
+                    }
+
+                }
+
+                if (elemNode.hasChildNodes()) {
+//recursive call if the node has child nodes
+                    printNodeList(elemNode.getChildNodes());
+                }
+            }
+        }
+//out of loops
+
+    }//end printNodeList
+
+    //method to get the lowest element from the array ychords (IP2)
+    public static void get_ip2(){
+        double min = ychords[0];
+        for (int i = 0; i < ychords.length; i++) {
+            //Compare elements of array with min
+            if(ychords[i] <min)
+                min = ychords[i];
+        }
+
+System.out.println("IP2 is " + min);
+    }
+//method to get the highest element from the array ychords (IP1)
+public static void get_ip1(){
+
+    double temp, size;
+    size = ychords.length;
+
+    for(int i = 0; i<size; i++ ){
+        for(int j = i+1; j<size; j++){
+            if(ychords[i]>ychords[j]){
+                temp = ychords[i];
+                ychords[i] = ychords[j];
+                ychords[j] = temp;
+            }
+        }
+    }
+    System.out.println("Ip1 is "+ychords[(int) (size-1)]);
+    }
+
+
+
+}
+
+
+
---
+++ src/hellofx/classes/seniorproject/HelloController.java
@@ -0,0 +1,108 @@
+package seniorproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Background;
+import javafx.stage.FileChooser;
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static seniorproject.HelloApplication.*;
+
+public class HelloController {
+
+    XYChart.Series series_wave1_plot = new XYChart.Series();
+    XYChart.Series series_wave1_plot2 = new XYChart.Series();
+    static int n = 0;
+    static int b = 0;
+    String[] xchords_to_string = new String[xchords.length];
+    @FXML
+    private LineChart<?, ?> chart;
+
+    @FXML
+    private CategoryAxis x;
+
+    @FXML
+    private NumberAxis y;
+
+    public void AddWave1(ActionEvent e) {
+        try {
+            //Open dialog box for user to select a file then put that path into variable selectedFile
+            FileChooser fc = new FileChooser();
+            File selectedFile = fc.showOpenDialog(null);
+            //Use the selected file and call printNodeList method in the main app to read through the xml
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.parse(selectedFile);
+            if (document.hasChildNodes()) {
+                printNodeList(document.getChildNodes());
+            }
+        } catch (Exception b) {}
+
+
+        //Convert xchords double array to a string array called xchords_to_string
+        for (int i = 0; i < xchords_to_string.length; i++)
+            xchords_to_string[i] = String.valueOf(xchords[i]);
+
+        //omit x values that are negative
+        for (int i = 1; i < xchords.length; i++) {
+            if (xchords[i] <=0 )//if its negative skip it
+            {
+                i++;//continue to next iteration
+            }
+
+            else
+                //add the xchord_to_string value and the corresponding ychord to the series AKA line plot
+                series_wave1_plot.getData().add(new XYChart.Data(xchords_to_string[i], ychords[i]));
+
+        }
+
+
+        series_wave1_plot2.getData().add(new XYChart.Data("4", 1));
+        series_wave1_plot2.getData().add(new XYChart.Data("6", 2));
+
+        series_wave1_plot2.setName("Wave 2");
+
+        series_wave1_plot.setName("Wave 1");
+        chart.setCreateSymbols(false); //hide dots
+        chart.getData().add(series_wave1_plot);
+        chart.getData().add(series_wave1_plot2);
+        //get_ip1();
+        //get_ip2();
+    }
+
+    public void clearchart(ActionEvent e) {
+
+        //////not working correctly
+        // //////when you clear and add another wave it adds the previous too even though you clear it
+        chart.getData().clear();
+        //chart.getData().remove(series_wave1_plot2);
+        //series_wave1_plot2.getData().removeAll(chart);
+        //series_wave1_plot.getData().removeAll(Collections.singleton(chart.getData().setAll()));
+        //chart.setAnimated(false);
+        //chart.setCreateSymbols(true);
+
+
+
+    }
+
+    public void Export(ActionEvent e) {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV FILES (*.csv)", "*.csv");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showSaveDialog(null);
+        System.out.print(file);
+    }
+
+
+}
